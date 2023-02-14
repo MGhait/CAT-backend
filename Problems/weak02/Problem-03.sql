5ca54fc330769f7a06887fe940fd5b3af8266fa4
Ghait <mosaad05019@gmail.com>
2023-02-14 23:55:22 +0200

    adding soluation of weak02 problems

---
+++ Problems/weak02/Problem-03.sql
@@ -0,0 +1,2 @@
+-- Query the total population of all cities in CITY where District is California.
+SELECT SUM(POPULATION) FROM CITY WHERE District ='California';
---
+++ Problems/weak02/problem-01.sql
@@ -0,0 +1,2 @@
+-- Query a list of CITY and STATE from the STATION table.
+SELECT CITY,STATE FROM STATION;
